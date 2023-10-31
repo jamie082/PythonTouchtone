9c673f5b51da2066032fc5256bf0dab624d9993b
Jamie Morrissey <jamiemorrissey02@gmail.com>
2023-10-31 19:20:21 +0000

    added new input code

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import time
 # Unit convertions
 #variable setting
 i = 1
-while i < 6: # loop 6 times
-  cat = input ("Which category would you like to convert? we support length(l) and Weight(w): ")
-  unit1 = input ("Which unit would you like to convert from: ")
-  unit2 = input ("Which unit would you like to convert to: ")
+cat = input ("Which category would you like to convert? we support length(l) and Weight(w): ")
+unit1 = input ("Which unit would you like to convert from: ")
+unit2 = input ("Which unit would you like to convert to: ")
 
 # Calculations
 
 if cat == "l": # Length
     if unit1 == "km" and unit2 == "m":
         km = float(input("Enter the value in km: "))
+        m = float(input("Enter the value in m: "))
         m = km * 1000
         print (f"{km} km is equal to {m} m")
     elif unit1 == "m" and unit2 == "km":
@@ -31,11 +31,12 @@ if cat == "l": # Length
           self.price = int(price)
 
 if cat == "w": # Width
-  if unit1 == "kg" and unit2 == "g":
-      kg = float(input("Enter the value in kg: "))
-      g = kg * 1000
-      print (f"{kg} kg is equal to {g} g")
-  elif unit1 == "g" and unit2 == "kg":
-      g = float(input("Enter the value in g: "))
-      kg = g / 1000
-      print (f"{g} g is equal to {kg} kg")
+    if unit1 == "kg" and unit2 == "g":
+        kg = float(input("Enter the value in kg: "))
+        g = float(input("Enter the value in g"))
+        g = kg * 1000
+        print (f"{kg} kg is equal to {g} g")
+    elif unit1 == "g" and unit2 == "kg":
+        g = float(input("Enter the value in g: "))
+        kg = g / 1000
+        print (f"{g} g is equal to {kg} kg")
