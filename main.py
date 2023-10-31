f84be7cd69b59523d537b052c812e676ddd7c346
Jamie Morrissey <jamiemorrissey02@gmail.com>
2023-10-31 19:49:28 +0000

    created class myClass

--- main.py
+++ main.py
@@ -17,23 +17,29 @@ if cat == "l": # Length
         print (f"{km} km is equal to {m} m")
     elif unit1 == "m" and unit2 == "km":
         m = float(input("Enter the value in m: "))
+        km = float(input("Enter the value in km: "))
         km = m / 1000
         print (f"{m} m is equal to {km} km")
 
     elif unit1 == "two":
-        class Shoes:
-          print('Company Message')
-          print('Quick Estimates')
+        class myClass:
 
-        def __init__ (self, Model, Description, price):
-          self.Model = Model
-          self.Description = Description
-          self.price = int(price)
+          def __init__ (self, Model, Description, price):
+            self.Model = Model
+            self.Description = Description
+            self.price = int(price)
 
+          def display_info(self):
+              print(f"Name: {self.name}")
+              print(f"Age: {self.age}")
+
+              # Accessing attributes and calling methods
+              obj.display_info()
+              
 if cat == "w": # Width
     if unit1 == "kg" and unit2 == "g":
         kg = float(input("Enter the value in kg: "))
-        g = float(input("Enter the value in g"))
+        g = float(input("Enter the value in g: "))
         g = kg * 1000
         print (f"{kg} kg is equal to {g} g")
     elif unit1 == "g" and unit2 == "kg":
