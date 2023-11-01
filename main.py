471733c29736c00be08e0e5024faeaf4ac59f8e0
jamie morrissey <jamiemorrissey02@gmail.com>
2023-11-01 18:13:00 +0000

    added while loop for 3 times

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 # Unit convertions
 #variable setting
 i = 1
-cat = input ("Which category would you like to convert? we support length(l) and Weight(w): ")
-unit1 = input ("Which unit would you like to convert from: ")
-unit2 = input ("Which unit would you like to convert to: ")
+while i < 3: # loop 3times
+  cat = input ("Which category would you like to convert? we support length(l) and   Weight(w) or (e) engineer: ")
+  unit1 = input ("Which unit would you like to convert from: ")
+  unit2 = input ("Which unit would you like to convert to: ")
 
-# Calculations
+  # Calculations
 
-if cat == "l": # Length
+  if cat == "l": # Length
     if unit1 == "km" and unit2 == "m":
         km = float(input("Enter the value in km: "))
         m = float(input("Enter the value in m: "))
@@ -20,23 +21,8 @@ if cat == "l": # Length
         km = float(input("Enter the value in km: "))
         km = m / 1000
         print (f"{m} m is equal to {km} km")
-
-    elif unit1 == "two":
-        class MyClass:
-
-          def __init__ (self, name, occupation):
-              self.name = name
-              self.occupation = occupation
               
-          def display_info(self):
-              print(f"Name: {self.name}")
-              print(f"Occupation: {self.occupation}")
-      
-        obj = MyClass("Jamie", "Software_Engineer")
-              # Accessing attributes and calling methods
-        obj.display_info()
-              
-if cat == "w": # Width
+  elif cat == "w": # Width
     if unit1 == "kg" and unit2 == "g":
         kg = float(input("Enter the value in kg: "))
         g = float(input("Enter the value in g: "))
@@ -46,3 +32,19 @@ if cat == "w": # Width
         g = float(input("Enter the value in g: "))
         kg = g / 1000
         print (f"{g} g is equal to {kg} kg")
+
+
+  elif cat == "e":
+      class MyClass:
+
+        def __init__ (self, name, occupation):
+          self.name = name
+          self.occupation = occupation
+
+      def display_info(self):
+          print(f"Name: {self.name}")
+          print(f"Occupation: {self.occupation}")
+
+          obj = MyClass("Jamie", "Software_Engineer")
+          # Accessing attributes and calling methods
+          obj.display_info()
